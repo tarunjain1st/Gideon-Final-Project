83138080e8f4c0a8fd2b277bb29dcfb50ff37216
Tarun Kumar Jain <tarunjain1st@gmail.com>
2021-02-26 22:33:54 +0530

    Update webcam.py

--- Raspberry_Pi/webcam.py
+++ Raspberry_Pi/webcam.py
@@ -6,11 +6,12 @@ server_socket.listen(5)
 
 def streamVideo():
     while True:
-    	client_socket,addr = server_socket.accept()
-    	if client_socket:
-    		vid = cv2.VideoCapture(0)
-    		while(vid.isOpened()):
-    			img,frame = vid.read()
-    			a = pickle.dumps(frame)
-    			message = struct.pack("Q",len(a))+a
-    			client_socket.sendall(message)
+        client_socket,addr = server_socket.accept()
+        if client_socket:
+            vid = cv2.VideoCapture(0)
+            while(vid.isOpened()):
+                img,frame = vid.read()
+                frame = cv2.resize(frame,(320,240))
+                a = pickle.dumps(frame)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
