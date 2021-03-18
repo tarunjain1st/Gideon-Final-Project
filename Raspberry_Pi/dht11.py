f87e32d1b47648d2583a6f0c5fead96cf4e9745c
Tarun Kumar Jain <tarunjain1st@gmail.com>
2021-03-18 23:18:18 +0530

    Update dht11.py

--- Raspberry_Pi/dht11.py
+++ Raspberry_Pi/dht11.py
@@ -1,11 +1,7 @@
 import Adafruit_DHT
 DHT_SENSOR=Adafruit_DHT.DHT11
 DHT_PIN=4
-data = {'temp':0,'hum':0}
 def getDht():
 	hum,temp=Adafruit_DHT.read(DHT_SENSOR,DHT_PIN)
 	if temp and hum:
-		data['temp']=temp
-		data['hum']=hum
-		print(data)
-		return data
+		return hum,temp
