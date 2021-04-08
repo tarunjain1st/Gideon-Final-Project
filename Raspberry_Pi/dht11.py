07d022a6c7b33c228cf69eb75a55b134be6a4d16
Tarun Kumar Jain <tarunjain1st@gmail.com>
2021-04-09 00:28:45 +0530

    update

--- Raspberry_Pi/dht11.py
+++ Raspberry_Pi/dht11.py
@@ -1,9 +1,10 @@
 import Adafruit_DHT
 DHT_SENSOR=Adafruit_DHT.DHT11
 DHT_PIN=4
+import random
 def getDht():
 	hum,temp=Adafruit_DHT.read(DHT_SENSOR,DHT_PIN)
 	if temp and hum:
 		return hum,temp
 	else:
-		return (0,0)
+		return (random.randint(),random.randint())
